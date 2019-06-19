0fc61fbec6660f76beae34f6b27c92a6e2de0bc2
xinquanatcolezhu <xinquan@colezhu.com>
2019-06-19 03:03:12 +0000

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,5 @@
 # --with-xpm=no --with-jpeg=no --with-gif=no --with-tiff=no --with-gnutls=no
 
-sudo apt-get install libtiff-dev libgtk-3-dev libjpeg-dev gnutls-dev libgif-dev -y
+sudo apt-get install libtiff-dev libgtk-3-dev libjpeg-dev gnutls-dev libgif-dev libxpm-dev -y
 
-./configure --with-mailutils --with-xpm=no
+./configure --with-mailutils
